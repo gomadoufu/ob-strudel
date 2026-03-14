40d675ed4b200ecddf3ecbe9c89d54deb6f1d7b4
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2026-03-14 23:33:57 +0900

    update

--- ob-strudel.el
+++ ob-strudel.el
@@ -1,35 +1,22 @@
 ;;; ob-strudel.el --- org-babel functions for strudel evaluation  -*- lexical-binding: t; -*-
-
-
 (require 'ob)
 (require 'strudel)
 
 (defun org-babel-execute:strudel (body params)
   "Execute a block of Strudel code with org-babel.
 BODY is the code block content, PARAMS are the header arguments."
-  ;; WebSocketサーバーが起動していない場合は起動
   (unless strudel-websocket-server
     (strudel-start))
-  
-  ;; 一時バッファでコードを実行
-  (with-temp-buffer
-    ;; JavaScriptモードを有効化（strudelはJavaScriptベース）
-    (when (fboundp 'rjsx-mode)
-      (rjsx-mode))
-    ;; strudel-modeを有効化
-    (strudel-mode 1)
-    ;; ソースブロックの内容を挿入
-    (insert body)
-    ;; strudelに送信
-    (strudel-run))
-  
-  ;; org-babelは結果を返すことを期待するが、
-  ;; strudelはブラウザ側で実行されるので結果は返せない
+  ;; with-temp-buffer ではなく名前付き永続バッファを使う。
+  ;; with-temp-buffer はフォーム終了後即 kill されるため WebSocket 送信が失敗する。
+  (let ((buf (get-buffer-create " *ob-strudel*")))
+    (with-current-buffer buf
+      (erase-buffer)
+      (insert body)
+      (strudel-run)))
+  ;; Strudel の出力はブラウザ側のため nil を返す
   "Code sent to Strudel")
 
-;; strudelをorg-babel言語リストに追加
 (add-to-list 'org-babel-load-languages '(strudel . t))
-
 (provide 'ob-strudel)
-
 ;;; ob-strudel.el ends here
